633a1f509ba82b6febb0dc0210a8828a5f8a87e9
avalanche05 <57392204+avalanche05@users.noreply.github.com>
2020-07-11 15:53:27 +0300

    a

--- src/main/java/com/goodcompany/salamport/SalamportApplication.java
+++ src/main/java/com/goodcompany/salamport/SalamportApplication.java
@@ -1,13 +1,35 @@
 package com.goodcompany.salamport;
 
+import com.goodcompany.salamport.services.FilesStorageService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
+import javax.annotation.Resource;
+
 @SpringBootApplication
-public class SalamportApplication {
+public class SalamportApplication implements CommandLineRunner {
+
+	@Resource
+	FilesStorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalamportApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
+	}
+
+
 
 }
